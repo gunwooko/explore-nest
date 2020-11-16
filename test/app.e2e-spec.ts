1bba6433f4306a9fdc643b581083a2aeeb536373
gunwooko <kkw722@gmail.com>
2020-11-16 15:13:20 +0900

    E2E Testing "/movies/:id" patch, delete

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -36,14 +36,28 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({ title: 'Test', year: 2020, genres: ['test'] })
         .expect(201);
     });
 
-    it('DELETE', () => {
+    it('POST 400 should send correct types of Movie', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', year: 2020, genres: ['test'], other: 'thing' })
+        .expect(400);
+    });
+
+    it('POST 400 should send all required types', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', genres: ['test'] })
+        .expect(400);
+    });
+
+    it('DELETE 404 Not Found', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
@@ -52,10 +66,27 @@ describe('AppController (e2e)', () => {
     it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
+
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/9999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+
+    it('PATCH 404', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/999')
+        .send({ title: 'Updated Test' })
+        .expect(404);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
